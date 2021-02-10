62afc3bafe4b601c66f4512cc345e55fadae6972
vladimir <vladi2909@outlook.com>
2021-02-10 22:51:57 +0300

    feat: add functional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ 
+  let map = new Map(bracketsConfig);
+  let array = [];
+
+  for (let i = 0; i < str.length; i++) {
+          str[i] === map.get(array[array.length - 1])
+                  ? array.pop()
+                  : array.push(str[i]);
+  }
+
+  return array.length === 0;
 }
+
